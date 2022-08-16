64f1772cb123e62c519879566f8ec9e6bf955695
sha241 <sha241@sfu.ca>
2022-08-16 13:14:43 -0700

    git test

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch('/api').then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data);
+      }
+    )
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Shade's Food Tracker</h1>
+      <div className='breakfast-box'>
+        <h2>
+          Breakfast
+          <span>!</span>
+
+        </h2>
+        <button>Gave</button>
+      </div>
+      <div className='churu-box'>
+        <h3>Churu</h3>
+        <button>Gave</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
