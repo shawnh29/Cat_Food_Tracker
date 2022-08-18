ef44ba9bd30f69462361db1f852f86d5d07eaf99
shawnh29 <Shawnhan20@gmail.com>
2022-08-18 01:31:18 -0700

    heroku deploy changes

--- server/server.js
+++ server/server.js
@@ -3,15 +3,14 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'build'))); 
+
 app.get("/api", (req, res) => {
     res.json({"users": ["user1", "user2", "user3"] });
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath)); 
 
 app.listen(port, () => {console.log("Listening on 5000")});
