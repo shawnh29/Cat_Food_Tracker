9267b55c99b570496c170e2169ca7763c1ff9a35
shawnh29 <Shawnhan20@gmail.com>
2022-08-18 01:09:05 -0700

    heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
 
 app.get("/api", (req, res) => {
     res.json({"users": ["user1", "user2", "user3"] });
 });
 
-app.listen(5000, () => {console.log("Listening on 5000")});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath)); 
+
+app.listen(port, () => {console.log("Listening on 5000")});
